b19439a061d6f7b3fe5ec85a67bb5e3f1800e13b
genueB <bay9895@gmail.com>
2022-12-18 23:04:31 +0900

    Initial Commit

---
+++ PracticeNote/PracticeNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeNoteApp.swift
+//  PracticeNote
+//
+//  Created by 배진우 on 2022/12/18.
+//
+
+import SwiftUI
+
+@main
+struct PracticeNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
